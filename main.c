59abaf80083380bfd8eed0b8c4e77cba9f1b9a88
Rianodji Dicard <dicardrianodji@gmail.com>
2025-03-28 15:47:37 +0100

    ajout de fonctionDidi dans main.c

--- main.c
+++ main.c
@@ -1,4 +1,10 @@
 #include <stdio.h>
+
+// fonction pour te saluer 
+void fonctionDidi(){
+   printf("Bonjour Paul \n");
+}
  int main(){
-    printf("Hello World");
+    printf("Hello World \n");
+    fonctionDidi();
  }
